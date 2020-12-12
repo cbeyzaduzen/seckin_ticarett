4ab85eb0f501e8590c74e919191c7de290fe4600
cbeyzaduzen <beyzaaduzen@gmail.com>
2020-12-12 23:09:50 +0300

    uygulama sayfaları oluşturuldu

---
+++ lib/sayfalar/bulasik.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class BulasikPage extends StatefulWidget {
+  @override
+  _BulasikPageState createState() => _BulasikPageState();
+}
+
+class _BulasikPageState extends State<BulasikPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/sayfalar/tv.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class TvPage extends StatefulWidget {
+  @override
+  _TvPageState createState() => _TvPageState();
+}
+
+class _TvPageState extends State<TvPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
